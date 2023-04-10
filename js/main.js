53bde03faf361753c746f480a04cea16e38ed293
Александр <savinovalexsandr93z@gmail.com>
2023-04-10 19:49:01 +0300

    Добавляет фильтры

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+import {init, getFilteredPictures} from './filters.js';
 import './form.js';
 import {getData} from './api.js';
 import {renderSimilarList} from './rendering.js';
 import './fullsize.js';
-import {showAlert} from './util.js';
+import {showAlert, debounce} from './util.js';
 
 
 try {
   const data = await getData();
-  renderSimilarList(data);
+  const debounceRenderSimilarList = debounce(renderSimilarList);
+  init(data, debounceRenderSimilarList);
+  renderSimilarList(getFilteredPictures());
 } catch (err) {
   showAlert(err.message);
 }
--- js/util.js
+++ js/util.js
@@ -31,4 +31,13 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomInteger, isEscapeKey, isEnterKey, showAlert};
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomInteger, isEscapeKey, isEnterKey, showAlert, debounce};
+
